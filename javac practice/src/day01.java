49788487074e2e4a90e02d445650cc423ff4062f
Yamini Jadhav <yaminijadhav2308@gmail.com>
2026-01-14 20:12:06 +0530

    day01

--- javac practice/src/day01.java
+++ javac practice/src/day01.java
@@ -1,9 +1,27 @@
-
-public class day01 {
+//hello world
+/*public class day01 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
 
 	}
 
+}*/
+
+//missing number...
+public class day01 {
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5,6,8,9,10};
+		int sum1=0;
+		for(int i=0;i<a.length;i++) {
+			sum1=sum1+a[i];
+		}
+		System.out.println("sum of element:" +sum1);
+		int sum2=0;
+		for(int i=1;i<=10;i++) {
+			sum2=sum2+i;
+		}
+		System.out.println("sum of range of array element:" +sum2);
+		System.out.println("missing no:" +(sum2-sum1));
+	}
 }
