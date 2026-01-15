a047a1c32cc7349afb14ba8e87c71e44d717f85d
Yamini Jadhav <yaminijadhav2308@gmail.com>
2026-01-15 18:19:02 +0530

    day02

---
+++ javac practice/src/day02.java
@@ -0,0 +1,22 @@
+//palindrome no...
+import java.util.Scanner;
+public class day02 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a num:");
+		int num=sc.nextInt();
+		int original=num;
+		int rev=0;
+		for(; num>0;num=num/10) {
+			int digit=num%10;
+			rev=rev*10+digit;
+		}
+		if(original==rev)
+			System.out.println("Palindrome no...");
+		else
+			System.out.println("Not a palindrome no..");
+
+	}
+
+}
