00bd49cff021a49a2a35dc1e419ccfec1cd3bcfc
Yamini Jadhav <yaminijadhav2308@gmail.com>
2026-01-16 12:16:26 +0530

    day03

--- javac practice/src/day03.java
+++ javac practice/src/day03.java
@@ -45,7 +45,7 @@ public class day03{
 	}
 }*/
 //Take a character and check whether it is a vowel or consonant...
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class day03{
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
@@ -65,5 +65,18 @@ public class day03{
 			System.out.println("it is not a alphabet..");
 		}
 		
+	}
+}*/
+//Take a number and check whether it is a multiple of 5 or not...
+import java.util.Scanner;
+public class day03{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a number...");
+		int num=sc.nextInt();
+		if(num%5==0)
+			System.out.println("divisible by 5..");
+		else
+			System.out.println("not divisible by 5..");
 	}
 }
