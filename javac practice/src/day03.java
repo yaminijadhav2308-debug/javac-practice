cb06062419975f97f67f7bdff559c119d3801d70
Yamini Jadhav <yaminijadhav2308@gmail.com>
2026-01-17 12:33:33 +0530

    day04

--- javac practice/src/day03.java
+++ javac practice/src/day03.java
@@ -68,15 +68,38 @@ public class day03{
 	}
 }*/
 //Take a number and check whether it is a multiple of 5 or not...
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class day03{
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter a number...");
 		int num=sc.nextInt();
 		if(num%5==0)
-			System.out.println("divisible by 5..");
+			System.o	y/ut.println("divisible by 5..");
 		else
 			System.out.println("not divisible by 5..");
 	}
+}*/
+
+//Armstring no...
+import java.util.Scanner;
+public class day03{
+	public static void main(String[] args) {
+	Scanner sc =new Scanner(System.in);
+	System.out.println("Enter a num...");
+	int num=sc.nextInt();
+	int original=num;
+	int sum=0;
+	while(num>0) {
+		int digit=num % 10;
+		sum=sum+ (digit*digit*digit);
+		num= num/10;
+	}
+	if(sum == original)
+		System.out.println(original+ "Armstrong num..");
+	else
+		System.out.println(original+ "not a armstrong num..");
+	sc.close();
+	}
 }
+
