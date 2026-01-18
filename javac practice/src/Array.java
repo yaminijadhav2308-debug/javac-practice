78a1d16777efec1a18929d448fe319052db003aa
Yamini Jadhav <yaminijadhav2308@gmail.com>
2026-01-18 20:48:12 +0530

    day04

--- javac practice/src/Array.java
+++ javac practice/src/Array.java
@@ -13,9 +13,20 @@ public class Array {
 }*/
 
 //Print numbers from 1 to 20 using a loop...
-public class Array{
+/*public class Array{
 	public static void main(String[] args) {
 		for(int i=1;i<=20;i++)
 			System.out.println(i);
+	}
+}*/
+
+//Print all even numbers between 1 and 100...
+public class Array{
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+			if(i%2==0)
+				System.out.println(i);
+			
+		}
 	}
 }
