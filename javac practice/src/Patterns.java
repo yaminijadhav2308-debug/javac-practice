f3f7674190f0d5b5116399073932aa3913e53e34
Yamini Jadhav <yaminijadhav2308@gmail.com>
2026-01-19 23:01:35 +0530

    Patterns

--- javac practice/src/Patterns.java
+++ javac practice/src/Patterns.java
@@ -41,17 +41,22 @@ op:
 	*****
 	*   *
 	*   *
-	***** */
+	*****   */
 
 
-public class Patterns {
+/*public class Patterns {
     public static void main(String[] args) {
         int n = 4;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-            System.out.println(); // move this here
+            System.out.println(); 
         }
     }
 }
+op:
+	*
+	**
+	***
+	****  */
