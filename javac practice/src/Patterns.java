2243267b9edacf8090bf6d78e441366c43d6ce28
Yamini Jadhav <yaminijadhav2308@gmail.com>
2026-01-20 18:37:22 +0530

    Patterns

--- javac practice/src/Patterns.java
+++ javac practice/src/Patterns.java
@@ -102,3 +102,84 @@ op:
 	   **
 	  ***
 	 ****   */
+
+/*public class Patterns{
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
+op:
+	1 
+	1 2 
+	1 2 3 
+	1 2 3 4 
+	1 2 3 4 5   */
+	
+/*public class Patterns{
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=n;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		} 
+	}
+}	
+op:
+	1 2 3 4 5 
+	1 2 3 4 
+	1 2 3 
+	1 2 
+	1     */
+/*public class Patterns{
+	public static void main(String[] args){
+		int n=5;
+		int number=1;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(number+" ");
+				number++;			}
+			System.out.println();
+		}
+		
+	}
+}
+op:
+	1 
+	2 3 
+	4 5 6 
+	7 8 9 10 
+	11 12 13 14 15   */
+
+
+/*public class Patterns{
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				int sum=i+j;
+				if(sum%2==0) {
+					System.out.print("1 ");
+				}
+				else {
+					System.out.print("0 ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
+op:
+	1 
+	2 3 
+	4 5 6 
+	7 8 9 10 
+	11 12 13 14 15   */
