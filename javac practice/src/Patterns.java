53d4eadf7415ffee43dee57639f206a09ccaeb43
Yamini Jadhav <yaminijadhav2308@gmail.com>
2026-01-21 21:06:05 +0530

    Patterns

--- javac practice/src/Patterns.java
+++ javac practice/src/Patterns.java
@@ -183,3 +183,78 @@ op:
 	4 5 6 
 	7 8 9 10 
 	11 12 13 14 15   */
+
+/*public class Patterns{
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			int spaces=2*(n-i);
+		    for(int j=1;j<=spaces;j++) 
+		    {
+		    	System.out.print(" ");
+		    }
+		    for(int j=1;j<=i;j++) {
+		    	System.out.print("*");
+		    }
+		    System.out.println();
+		
+				
+		}
+		for(int i=n;i>=1;i--) {
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			int spaces=2*(n-i);
+		    for(int j=1;j<=spaces;j++) 
+		    {
+		    	System.out.print(" ");
+		    }
+		    for(int j=1;j<=i;j++) {
+		    	System.out.print("*");
+		    }
+		    System.out.println();
+		
+				
+		}
+		
+		
+	}
+}
+
+op:
+	*      *
+	**    **
+	***  ***
+	********
+	********
+	***  ***
+	**    **
+	*      *  */
+
+
+public class Patterns{
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+op:
+    *****
+   *****
+  *****
+ *****
+*****
