a9ed5102fcd738986cb85c006c66b428593b7aa4
Yamini Jadhav <yaminijadhav2308@gmail.com>
2026-01-22 21:00:14 +0530

    Patterns

--- javac practice/src/Patterns.java
+++ javac practice/src/Patterns.java
@@ -259,7 +259,7 @@ op:
  *****
 *****    */
  
-public class Patterns{
+/*public class Patterns{
 	public static void main(String[] args) {
 		int n=5;
 		for(int i=1;i<=n;i++) {
@@ -278,6 +278,32 @@ op:
    2 2 
   3 3 3 
  4 4 4 4 
-5 5 5 5 5 
+5 5 5 5 5 */
 
+import java.util.Scanner;
+public class Patterns{
+        public static void main(String[] args){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter your number...");
+            int n=sc.nextInt();
+            for(int i=1;i<=n;i++) {
+            	for(int j=1;j<=n-i;j++) {
+            		System.out.print(" ");
+            	}
+            	for(int j=1;j<=2*i-1;j++) {
+            		System.out.print("*");
+            	}
+            	System.out.println();
+            }
+            for(int i=n;i>=1;i--) {
+            	for(int j=1;j<=n-i;j++) {
+            		System.out.print(" ");
+            	}
+            	for(int j=1;j<=2*i-1;j++) {
+            		System.out.print("*");
+            	}
+            	System.out.println();
+            }
+        }
+}
 	
